4f8961da66af54339c59698b1cc4ea8d95ab6043
Yujie Wen <yjwen.ty@qq.com>
2020-10-24 23:12:22 +0800

    Update ergodox EZ layout. Rearranged Fx keys. Fast key for Alt+Tab

--- keyboards/ergodox_ez/keymaps/yj/keymap.c
+++ keyboards/ergodox_ez/keymaps/yj/keymap.c
@@ -21,13 +21,96 @@ enum custom_keycodes {
   EPRM,
 };
 
+// Reversed ergodox layer
+#define LAYOUT_xodogre(                         \
+  k00,k01,k02,k03,k04,k05,k06,                  \
+  k10,k11,k12,k13,k14,k15,k16,                  \
+  k20,k21,k22,k23,k24,k25,                      \
+  k30,k31,k32,k33,k34,k35,k36,                  \
+  k40,k41,k42,k43,k44,                          \
+                      k55,k56,                  \
+                          k66,                  \
+                  k74,k75,k76,                  \
+  k07,k08,k09,k0A,k0B,k0C,k0D,                  \
+  k17,k18,k19,k1A,k1B,k1C,k1D,                  \
+      k28,k29,k2A,k2B,k2C,k2D,                  \
+  k37,k38,k39,k3A,k3B,k3C,k3D,                  \
+          k49,k4A,k4B,k4C,k4D,                  \
+  k57,k58,                                      \
+  k67,                                          \
+  k77,k78,k79 )                                 \
+  LAYOUT_ergodox                                \
+ (                                              \
+  k0D,k0C,k0B,k0A,k09,k08,k07,                  \
+  k1D,k1C,k1B,k1A,k19,k18,k17,                  \
+  k2D,k2C,k2B,k2A,k29,k28,                      \
+  k3D,k3C,k3B,k3A,k39,k38,k37,                  \
+  k4D,k4C,k4B,k4A,k49,                          \
+                      k58,k57,                  \
+                          k67,                  \
+                  k79,k78,k77,                  \
+  k06,k05,k04,k03,k02,k01,k00,                  \
+  k16,k15,k14,k13,k12,k11,k10,                  \
+      k25,k24,k23,k22,k21,k20,                  \
+  k36,k35,k34,k33,k32,k31,k30,                  \
+          k44,k43,k42,k41,k40,                  \
+  k56,k55,                                      \
+  k66,                                          \
+  k76,k75,k74 )
+
+  
+                 
+                  
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_ergodox(KC_MINUS,KC_0,KC_9,KC_8,KC_7,KC_6,KC_RGUI,KC_EQUAL,KC_P,KC_O,KC_I,KC_U,KC_Y,TG(1),KC_RCTRL,LT(1,KC_SCOLON),KC_L,KC_K,KC_J,KC_H,LSFT_T(KC_BSLASH),KC_SLASH,KC_DOT,KC_COMMA,KC_M,KC_N,KC_RBRACKET,LT(1,KC_NO),KC_BSLASH,KC_RBRACKET,KC_LBRACKET,RALT_T(KC_QUOTE),KC_DOWN,KC_UP,KC_HOME,KC_SPACE,KC_BSPACE,KC_END,KC_LGUI,KC_5,KC_4,KC_3,KC_2,KC_1,KC_ESCAPE,TG(1),KC_T,KC_R,KC_E,KC_W,KC_Q,KC_GRAVE,KC_G,KC_F,KC_D,KC_S,KC_A,LCTL_T(KC_CAPSLOCK),KC_LBRACKET,KC_B,KC_V,KC_C,KC_X,KC_Z,KC_LSHIFT,KC_LALT,KC_LCTRL,KC_INSERT,KC_DELETE,MO(1),KC_RIGHT,KC_LEFT,KC_PGUP,KC_PGDOWN,KC_ENTER,KC_TAB),
+[0] = LAYOUT_xodogre
+(
+ // Left hand
+ KC_ESCAPE,       KC_1,       KC_2,       KC_3,      KC_4,     KC_5,     TG(1),
+ KC_GRAVE,        KC_Q,       KC_W,       KC_E,      KC_R,     KC_T,     MO(1),
+ LCTL_T(KC_CAPS), KC_A,       KC_S,       KC_D,      KC_F,     KC_G,     
+ KC_LSHIFT,       KC_Z,       KC_X,       KC_C,      KC_V,     KC_B,     KC_LBRACKET,  
+ KC_LGUI,         KC_F1,      KC_F2,      KC_F3,     KC_LALT,
+ // Left thumb
+                                                               KC_HOME,  KC_END,
+                                                                         KC_PGUP,
+                                                     KC_TAB,   KC_ENTER, KC_PGDOWN,
+ 
 
-  [1] = LAYOUT_ergodox(KC_ESCAPE,KC_F12,KC_F11,KC_F10,KC_F9,KC_F8,KC_F7,KC_TRANSPARENT,KC_EXLM,KC_AT,KC_RCBR,KC_LCBR,KC_PIPE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_DLR,KC_RPRN,KC_LPRN,KC_GRAVE,KC_TRANSPARENT,KC_PERC,KC_CIRC,KC_RBRACKET,KC_LBRACKET,KC_TILD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F6,KC_F5,KC_F4,KC_F3,KC_F2,KC_F1,KC_F11,KC_TRANSPARENT,KC_UP,KC_7,LALT(LCTL(KC_F3)),LALT(LCTL(KC_F2)),LALT(LCTL(KC_F1)),KC_F12,KC_DOWN,KC_4,KC_5,KC_6,KC_PLUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_AMPR,KC_1,KC_2,KC_3,KC_BSLASH,KC_TRANSPARENT,KC_TRANSPARENT,KC_DOT,KC_0,KC_EQUAL,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
+ // Right hand
+ TG(1),           KC_6,       KC_7,              KC_8,             KC_9,         KC_0,       KC_MINUS,
+ MO(1),           KC_Y,       KC_U,              KC_I,             KC_O,         KC_P,       KC_EQUAL,
+                  KC_H,       KC_J,              KC_K,             KC_L,         KC_SCOLON,  KC_RCTRL,         
+ KC_RBRACKET,     KC_N,       KC_M,              KC_COMMA,         KC_DOT,       KC_SLASH,   RSFT_T(KC_BSLASH),  
+                              RALT_T(KC_LEFT),   KC_UP,            KC_DOWN,      KC_RIGHT,   KC_RGUI,
 
-  [2] = LAYOUT_ergodox(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_UP,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_BTN1,KC_MS_BTN2,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT,KC_TRANSPARENT,KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN,KC_AUDIO_MUTE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_WWW_BACK),
+ // Right thumb
+ KC_INSERT,       KC_DELETE,    
+ RSFT(RALT(KC_TAB)),
+ RALT(KC_TAB),    KC_BSPACE,  KC_SPACE
+ ),
 
+  [1] = LAYOUT_xodogre
+(
+ // Left
+ KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
+ KC_TRNS, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_TRNS,
+ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                              KC_TRNS, KC_TRNS,
+                                                       KC_TRNS,
+                                     KC_TRNS, KC_TRNS, KC_TRNS,
+
+ // Right
+ KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+ KC_TRNS, KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_TRNS,
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+ KC_TRNS, KC_TRNS,
+ KC_TRNS,
+ KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
